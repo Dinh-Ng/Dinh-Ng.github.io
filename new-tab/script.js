06d40282e9dd5b83f4be2ac61c70b600db758a9f
DiNg <dinhquangnguyen1997@gmail.com>
2024-10-09 00:27:14 +0700

    new-tab: button => a

--- new-tab/script.js
+++ new-tab/script.js
@@ -3,30 +3,48 @@ const container = document.getElementById("container");
 
 // Mảng chứa dữ liệu các item (bạn có thể thay đổi dữ liệu này)
 const items = [
-    { text: "Truyenqq", link: "https://truyenqqto.com" },
-    { text: "HoyoMap", link: "https://act.hoyolab.com/ys/app/interactive-map/index.html" },
-    { text: "Seelie", link: "https://seelie.me/planner" },
-    { text: "Seelie ZZZ", link: "https://zzz.seelie.me/planner" },
-    { text: "HoyoMap", link: "https://act.hoyolab.com/ys/app/interactive-map/index.html" },
-    { text: "Genshin Material", link: "https://docs.google.com/spreadsheets/d/19FZsuNH-_1vd5Np5OFKWJWVOQXBpE0D-GLlIjxdAuAM/edit?ouid=110511358014061828669&usp=sheets_home&ths=true" },
-    { text: "Genshin Primogem Counter", link: "https://docs.google.com/spreadsheets/d/1ziDADtCxijE-pJROcL4qoVu8jryYIwPFWbyeKSXG5vA" },
-    { text: "", link: "" },
+  { text: "Truyenqq", link: "https://truyenqqto.com" },
+  {
+    text: "HoyoMap",
+    link: "https://act.hoyolab.com/ys/app/interactive-map/index.html",
+  },
+  { text: "Seelie", link: "https://seelie.me/planner" },
+  { text: "Seelie ZZZ", link: "https://zzz.seelie.me/planner" },
+  {
+    text: "GI Material",
+    icon: "https://ssl.gstatic.com/docs/spreadsheets/spreadsheets_2023q4.ico",
+    link: "https://docs.google.com/spreadsheets/d/19FZsuNH-_1vd5Np5OFKWJWVOQXBpE0D-GLlIjxdAuAM/edit?ouid=110511358014061828669&usp=sheets_home&ths=true",
+  },
+  {
+    text: "GI Primogem Counter",
+    icon: "https://ssl.gstatic.com/docs/spreadsheets/spreadsheets_2023q4.ico",
+    link: "https://docs.google.com/spreadsheets/d/1ziDADtCxijE-pJROcL4qoVu8jryYIwPFWbyeKSXG5vA",
+  },
+  { text: "", link: "" },
 ];
 
 // Tạo các button và append vào container
-items.forEach(item => {
-    const button = document.createElement("button");
+items.forEach((item) => {
+  const button = document.createElement("a");
+  //   button.textContent = item.text;
 
-    // Tạo element image và append vào button
-    const image = document.createElement("img");
-    image.src = "http://www.google.com/s2/favicons?domain=" + item.link;
-    button.appendChild(image);
-    button.textContent = item.text;
+  // Tạo element image và append vào button
+  const image = document.createElement("img");
+  image.src =
+    item.icon ?? "http://www.google.com/s2/favicons?domain=" + item.link;
+  button.appendChild(image);
 
-    // Thêm sự kiện onclick để chuyển hướng
-    button.onclick = function () {
-        window.location.href = item.link;
-    };
+  // Create text button
+  const text = document.createElement("p");
+  text.textContent = item.text;
+  button.appendChild(text);
 
-    container.appendChild(button);
+  // Thêm sự kiện onclick để chuyển hướng
+  button.href = item.link
+
+  const parent = document.createElement("div");
+  parent.id = "bookmark";
+  parent.appendChild(button);
+
+  container.appendChild(parent);
 });
